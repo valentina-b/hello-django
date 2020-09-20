f33d4aa7fff9869617ff6a88790bae102d66fb46
valentina-b <valentina.bedi@gmail.com>
2020-09-20 16:48:52 +0000

    Coverage and add missing tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,5 +7,9 @@ from .models import Item
 class TestDjangoTestModels(TestCase):
 
     def test_done_defaults_to_false(self):
-        item = Item.objects.create(name='est Todo Item')
+        item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
